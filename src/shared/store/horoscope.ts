11b98c4b0b6640017fb4eea92ea048108b6cfac5
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-21 23:28:24 +0300

    add BackButton close tg if popup is not open

--- src/shared/store/horoscope.ts
+++ src/shared/store/horoscope.ts
@@ -1,15 +1,18 @@
 export const useHoroscopeStore = defineStore("horoscope", () => {
   const text = ref<string>("");
+  const isOpen = ref<boolean>(false);
 
   async function open(sign: string) {
+    isOpen.value = true;
     const data = await fetchSignData(sign);
     text.value = data.horoscope;
     window.myDialog.showModal();
   }
 
   function close() {
+    isOpen.value = false;
     window.myDialog.close();
   }
 
-  return { text, open, close };
+  return { text, isOpen, open, close };
 });
