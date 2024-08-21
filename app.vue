45fdeceb66efe2967427b07c5d2614c0f6fb9c2c
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-08-21 22:51:25 +0300

    add check user language in app.vue

--- app.vue
+++ app.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { setLocale } = useI18n();
+const { useWebApp } = await import("vue-tg");
+const { initDataUnsafe } = useWebApp();
+
+onMounted(() => {
+  const language_code = initDataUnsafe.user.language_code;
+  setLocale(language_code == "en" || "ru" ? language_code : "en");
+});
+</script>
 
 <template>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
-
-<style lang="scss" scoped></style>
